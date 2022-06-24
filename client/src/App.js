ea3fc762dbfe4a1b8d4359f0c46f17cf5abfb120
Colby Patterson <cpatt@byui.edu>
2022-06-24 14:39:17 -0600

    Added routes to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/shared/Navbar'
+import Home from './components/shared/Home'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import NoMatch from './components/shared/NoMatch'
+import FetchUser from './components/auth/FetchUser';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import UserAccount from './components/shared/UserAccount';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <>
+      <FetchUser>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path='/account' element={<UserAccount />} />
+          </Route>
+          <Route path='/*' element={<NoMatch />} />
+        </Routes>
+        </FetchUser>
+      </>
+    </>
   );
 }
 

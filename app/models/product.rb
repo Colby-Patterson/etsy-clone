46f9c5f3507534fd71dae98c557c1d4e82957fb6
Colby Patterson <cpatt@byui.edu>
2022-06-28 16:04:36 -0600

    Products backend tested and working

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :seller
+
+  def self.for_sale
+    Product.find_by_sql("SELECT p.id AS product_id, price, description, category, first_name,
+    last_name, email, s.id AS seller_id 
+    FROM products AS p
+    INNER JOIN sellers AS s
+    ON p.seller_id = s.id
+    WHERE p.sold = false;")
+  end
 end

b569fea5a55055cf5eb61f646ea55aaf6d98c128
Colby Patterson <cpatt@byui.edu>
2022-06-28 15:37:55 -0600

    Routes debugging

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/products', to: 'products#index'
+  namespace :api do
+    get '/products', to: 'products#index'
+  end
 end

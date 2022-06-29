ea03cc896dec7082079989f6bec49ddb339e80b0
Colby Patterson <cpatt@byui.edu>
2022-06-29 10:01:34 -0600

    Created products for sale component

--- client/src/pages/ProductsForSale.js
+++ client/src/pages/ProductsForSale.js
@@ -7,9 +7,16 @@ const ProductsForSale = () => {
     getProductProperties()
   }, [])
 
+  const normalizeRawData = (rawData) => {
+    const sellerIDs = rawData.map((rd)=> rd.seller_id)
+    const uniqueIDs = [...new Set(sellerIDs)]
+    console.log(uniqueIDs)
+  }
+
   const getProductProperties = async () => {
     try {
       let res = await axios.get("/api/products")
+      normalizeRawData(res.data)
       setProductProperties(res.data)
     } catch(err) {
       alert("Error occurred in getProductProperties")

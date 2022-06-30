d41a57cb2b05be0d7eab1afccebab79759fb97cb
Colby Patterson <cpatt@byui.edu>
2022-06-30 16:45:11 -0600

    Show product by category

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,4 +9,16 @@ class Product < ApplicationRecord
     ON p.seller_id = s.id
     WHERE p.sold = false;")
   end
+
+  def self.categories
+    categories = Product.find_by_sql("SELECT DISTINCT category FROM products")
+    categories.map do |c|
+      c.category
+    end
+  end
+
+  def self.by_category(category)
+    Product.find_by_sql(["SELECT id, description FROM products
+    WHERE sold = false AND LOWER(category) = ?", category.downcase])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api do
     get '/products', to: 'products#index'
+    get '/categories', to: 'products#categories'
+    get '/categories/:category', to: 'products#by_category'
   end
 end
